23b350e182e21688d0e033ced646ea1ece363fef
Jonathan Costa <jhoncosta2@gmail.com>
2026-04-20 11:55:18 -0300

    add struct cityCart to superTrunfo_c/test.c

--- semester_1/linguagem_c/superTrunfo_c/test.c
+++ semester_1/linguagem_c/superTrunfo_c/test.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-//cadastrar as cartas
+//molde das cartas
+struct cityCard {
+    int carNumber;
+    char state;
+    char cityName[20];
+    char cardCod[3];
+    //attributes
+    int touristAttractions;
+    unsigned int long population;
+    float area;
+    float pib;
+    float pibPerCapita;
+    float populationDensity;
+};
+
 int main(int argc, char const *argv[])
 {
     
     return 0;
 }
+
+//criar menu
