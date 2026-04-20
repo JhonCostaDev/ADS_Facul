755610f9b34605022280b206bbfc052c88110246
Jonathan Costa <jhoncosta2@gmail.com>
2026-04-20 12:28:48 -0300

    add getUserInput() function v1 to superTrunfo_c/test.c

--- semester_1/linguagem_c/superTrunfo_c/test.c
+++ semester_1/linguagem_c/superTrunfo_c/test.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+//TODO: implement clean screen function
 //molde das cartas
 struct cityCard {
     int carNumber;
@@ -15,10 +15,49 @@ struct cityCard {
     float populationDensity;
 };
 
+//prototypes
+int mainMenu();
+int getUserInput();
+
+//main
 int main(int argc, char const *argv[])
 {
-    
+    int test = mainMenu();
+    printf("%d\n", test);
     return 0;
 }
 
 //criar menu
+/*
+1 - novo jogo
+    - Inicia um novo jogo contra a maquina
+2 - ver regras do jogo
+    - Exibir as regras do jogo
+3 - cadastrar cartas
+    - Inserir novas castas no baralho
+4 - ver cartas cadastradas
+- Exibir a quantidades de cartas        cadastradas e poder ver carta a carta
+0 - sair
+    - Sair do programa
+*/
+int mainMenu() {
+    printf("\t===== Super Trunfo Cidades =====\n");
+    printf("\t\t1 - Novo Jogo\n\t\t2 - Ver regras do Jogo\n\t\t3 - Cadastrar Nova Carta\n\t\t4 - Ver Cartas Cadastradas\n\t\t0 - Sair do Game\n");
+    int option = getUserInput();
+    return option;
+}
+
+int getUserInput() {
+    int value;
+    printf("Escolha uma das opções acima:\n");
+
+    //loop
+    while (scanf("%d", &value) != 1 || value < 0 || value > 4) {
+        printf("Error: Digite apenas números entre 0 e 4 (inclusos): \n");
+
+        //clean buffer
+        while (getchar() != '\n');
+    }
+
+    return value;
+}
