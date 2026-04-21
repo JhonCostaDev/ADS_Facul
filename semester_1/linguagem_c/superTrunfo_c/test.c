5142fd9a56635499899cccb1d6525b7478bf8e00
Jonathan Costa <jhoncosta2@gmail.com>
2026-04-21 20:13:33 -0300

    Implement listCards(), pauseEnter() functions and fix some bugs on createNewCard() function on linguagem_c/superTrunfo_c/test.c

--- semester_1/linguagem_c/superTrunfo_c/test.c
+++ semester_1/linguagem_c/superTrunfo_c/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-//TODO: implement option 2 register new cards
+//TODO: corrigir erros nas funções cadastrar nova carta e exibir cartas
+//Salvar cartas cadastradas em arquivo
 
 //Consts
 #define MAX_CARDS 100
@@ -26,6 +27,8 @@ int mainMenu();
 int getUserInput(char msg[], int min, int max);
 void cleanScreen();
 void createNewCard(struct cityCard card[], int *numberOfCards);
+void listCards(struct cityCard card[], int arraysize);
+void pauseEnter();
 
 //main
 int main(int argc, char const *argv[]) {
@@ -49,8 +52,10 @@ int main(int argc, char const *argv[]) {
             createNewCard(cityCards, &numberOfCards);
             break;
         
-        case 0:
-            printf("exit%d\n", option);
+        case 4:
+            printf("Listar cartas%d\n", option);
+            listCards(cityCards, numberOfCards);
+            pauseEnter();
             break;
         default:
             break;
@@ -107,6 +112,8 @@ void cleanScreen() {
 }
 
 //This function create a new card
+//TODO: exibir mensagem de inserção success
+//TODO: super power está exibindo os valores de área
 void createNewCard(struct cityCard card[], int *numberOfCards){
     //int cardNumber = numberOfCards;
     if(*numberOfCards < MAX_CARDS) {
@@ -143,4 +150,23 @@ void createNewCard(struct cityCard card[], int *numberOfCards){
         card[*numberOfCards].superPower = (long double) card[*numberOfCards].population + card[*numberOfCards].area + card[*numberOfCards].pib + card[*numberOfCards].touristAttractions + card[*numberOfCards].pibPerCapita + (1/card[*numberOfCards].populationDensity);
         (*numberOfCards)++;
     }
+}
+
+void listCards(struct cityCard card[], int arraysize) {//TODO: Verificar especificadores corretos Unsigend long
+    printf("===== Cartas cadastradas: %d =================\n", arraysize);
+    
+    for(int i = 0; i < arraysize; i++) {
+        printf("Número da carta: %d\nEstado(UF):%s\nCidade: %s\nCódigo da Carta: %s\nPopulação: %ld\nÁrea: %.2f\nPIB: %.2f\nPIB per Capta: %.2f\nDensidade Populacional: %.2f\nNúmero de Pontos Turísticos: %d\nSuper Poder: %.2f\n",card[i].cardNumber,card[i].state, card[i].cityName, card[i].cardCod, card[i].population, card[i].area, card[i].pib, card[i].pibPerCapita, card[i].populationDensity, card[i].touristAttractions, card[i].superPower);
+    }
+    printf("======================\n");
+}
+
+void pauseEnter() {
+    int c;
+    printf("\nPressione Enter para continuar ...");
+    fflush(stdout); // Flush the output buffer to ensure the prompt is displayed before waiting for input
+
+    while((c = getchar()) != '\n' && c != EOF); //  Wait for the user to press Enter (newline character) and ignore any other input until Enter is pressed
+
+    getchar(); // Consume the newline character left in the input buffer after pressing Enter to prevent it from affecting subsequent input operations
 }
