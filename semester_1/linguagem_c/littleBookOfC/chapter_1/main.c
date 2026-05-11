713df08a354c26e472b4a118d7364388e67f4d9d
Jonathan Costa <jhoncosta2@gmail.com>
2026-05-11 11:40:48 -0300

    add new branch - littleBookOfC and its files

---
+++ semester_1/linguagem_c/littleBookOfC/chapter_1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void greet(void);
+
+int main(int argc, char const *argv[]) {
+    greet();
+    //return 0;
+}
---
+++ semester_1/linguagem_c/littleBookOfC/chapter_1/my_project/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "greet.h"
+
+int main(void) {
+    greet("Jhon");
+    return 0;
+}
